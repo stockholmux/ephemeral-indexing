979d0d790b86fd6034cdb72e1e8b07a84640889d
Kyle <kyle@kylejdavis.com>
2020-04-16 14:57:30 -0600

    first stream

--- index.js
+++ index.js
@@ -1,28 +1,49 @@
 
 const 
-  commander = require('commander'),
-  fastify   = require('fastify')({
+  commander       = require('commander'),
+  fastify         = require('fastify')({
     //logger: true
   }),
-  path      = require('path'),
-  products  = require('./products.js'),
-  users     = require('./users.js'),
-  purchases = require('./purchases.js');
+  redis           = require('redis'),
+  path            = require('path'),
+  { promisify }   = require('util'),
+  fnv1a           = require('@sindresorhus/fnv1a'),
+  products        = require('./products.js'),
+  users           = require('./users.js'),
+  purchases       = require('./purchases.js');
 
+redis.add_command('ft.search');
+redis.add_command('ft.add');
+redis.add_command('ft.create');
+redis.add_command('ft.info'); // do we need this?
+  
 fastify.register(require('fastify-static'), {
   root: path.join(__dirname, 'public')
 });
 commander
   .requiredOption('-i, --input <path>', 'Path to JSON file product metadata')
-  .requiredOption('-c, --count <count of products>', 'Number of products to include');
+  .requiredOption('-c, --count <count of products>', 'Number of products to include')
+  .requiredOption('-h, --redishost <Redis host IP>', 'Host for Redis')
+  .requiredOption('-p, --redisport <port for Redis>','Port for Redis')
+  .option('-a, --redisauth <redis password>','Password for Redis instance');
 
 const 
-  args = commander.parse(process.argv);
+  args    = commander.parse(process.argv),
+  client  = redis.createClient({
+    host      : args.redishost,
+    port      : args.redisport,
+    password  : args.redispauth
+  });
+
+// create Async functions out of Redis Commands
+const
+  ft_create = promisify(client.ft_create).bind(client),
+  ft_add    = promisify(client.ft_add).bind(client),
+  ft_search = promisify(client.ft_search).bind(client);
 
 console.log('loading products...');
 products.load(args.input, args.count);
 console.log('loaded.');
-
 
 const byIdentifer = (src,param) => async (request) => {
   let one = src.get(request.params[param]);
@@ -44,6 +65,52 @@ fastify.get('/user/:start/:end',list(users));
 fastify.get('/user/:email',byIdentifer(users,'email'));
 fastify.put('/user/:email',async (request) => {
   // User login
+  const
+    hashedEmail     = fnv1a(request.params.email),
+    purchaseHistory = await purchases.history(request.params.email),
+    indexName       = `user:${hashedEmail}`;
+
+  let createResult;
+  const indexPurchases = async (history) =>
+  history.map(
+    (item) => {
+      let details = products.get(item[1]);
+
+      return ft_add(
+        indexName,
+        fnv1a(indexName+item),
+        1,
+        'FIELDS',
+        'purchased', item[0],
+        'title', details.title,
+        'price', details.price,
+        'desc', details.description,
+        'itemid', item[1]
+      )
+    }
+  );
+
+
+  let userPurchases;
+  try {
+    createResult = await ft_create(
+      indexName,    //name of index
+      'TEMPORARY','60',
+      'SCHEMA',
+        'purchased', 'NUMERIC',
+        'title','TEXT',
+        'desc','TEXT',
+        'price', 'NUMERIC',
+        'itemid','TEXT'
+    );
+    userPurchases = await indexPurchases(purchaseHistory);
+  } catch(err) {
+    if (!String(err).includes('Index already exists')) {
+      throw err;
+    }
+  }
+  
+ 
   return { status : 'OK' };
 });
 
